357329a99dfcb0888d0475701f11d4dade293da7
KQTENQK <67883180+KQTENQK@users.noreply.github.com>
2021-04-01 01:00:15 +0300

    Sygnalization
    Added volume changing while syngalizing
    Removed stop delay

--- Assets/Scripts/Sygnalization.cs
+++ Assets/Scripts/Sygnalization.cs
@@ -1,35 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Sygnalization : MonoBehaviour
 {
+    [SerializeField] float _increaseVolumeRate = 0.2f;
+
     private AudioSource _audioSource;
+    private const float _startVolume = 0.1f;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.TryGetComponent<PlayerMovement>(out PlayerMovement player))
         {
             _audioSource.Play();
+            StartCoroutine(Sygnalize());
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collider)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collider.gameObject.TryGetComponent<PlayerMovement>(out PlayerMovement player))
+        if (collision.gameObject.TryGetComponent<PlayerMovement>(out PlayerMovement player))
         {
-            StartCoroutine(StopSygnalizeWithDelay());
+            StopCoroutine(Sygnalize());
+            _audioSource.Stop();
         }
     }
 
-    private IEnumerator StopSygnalizeWithDelay()
+    private IEnumerator Sygnalize()
     {
-        yield return new WaitForSeconds(0.5f);
-        _audioSource.Stop();
+        float currentVolume = _startVolume;
+        float minVolume = 0f;
+        float targetVolume = 1f;
+        
+        while (true)
+        {
+            currentVolume = Mathf.MoveTowards(currentVolume, targetVolume, _increaseVolumeRate * Time.deltaTime);
+            _audioSource.volume = currentVolume;
+
+            if (currentVolume >= 1 || currentVolume <= 0.1f)
+            {
+                float temp = targetVolume;
+                targetVolume = minVolume;
+                minVolume = temp;
+            }
+
+            yield return null;
+        }
     }
 
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = _startVolume;
     }
 }
